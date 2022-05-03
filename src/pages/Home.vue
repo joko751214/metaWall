228f9ef72b3e3f97129bb43efa850da11ca340d3
joko751214 <joko751214@gmail.com>
2022-05-03 22:54:13 +0800

    header 切版

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h1>首頁</h1>
+    <header
+      class="flex justify-around items-center bg-white border-b-2 border-black bottom-shadow py-3"
+    >
+      <p class="font-special text-2xl">MetaWall</p>
+      <div class="flex items-center">
+        <img class="w-8 mr-2.5" src="@/assets/user.png" alt="user-image" />
+        <span href="#" class="border-b border-black font-secondary font-bold px-1">Member</span>
+      </div>
+    </header>
   </div>
 </template>
 
