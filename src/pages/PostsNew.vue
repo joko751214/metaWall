0dfe64080cee179047ed7b6ccc3e87d82438ffc5
joko751214 <joko751214@gmail.com>
2022-05-05 04:42:00 +0800

    張貼動態頁面切板，製作 double border 效果

---
+++ src/pages/PostsNew.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-2/5 pr-16">
+    <div
+      class="bg-white text-xl text-center py-4 border-2 border-black font-secondary font-bold double-bolder"
+    >
+      張貼動態
+    </div>
+  </div>
+</template>
+
+<script setup></script>
