c4ae55ce9cc9ba650131ae96293f79b090863ddb
joko751214 <joko751214@gmail.com>
2022-05-04 23:06:51 +0800

    安裝 font-awsome 套件，並註冊至全局

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
 import './sass/index.sass';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(fab, fas, far);
 
 const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.config.productionTip = false;
 app.use(router);
 app.mount('#app');
