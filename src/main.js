70d65a69f06dfe6980ec1509ba0ecb25fd095344
joko751214 <joko751214@gmail.com>
2022-05-09 04:01:47 +0800

    設置環境變數與 proxy

--- src/main.js
+++ src/main.js
@@ -7,11 +7,16 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import { createPinia } from 'pinia';
 
 library.add(fab, fas, far);
 
 const app = createApp(App);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.config.productionTip = false;
+app.use(createPinia());
 app.use(router);
+app.use(ElementPlus);
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    resolve: {
+      // 路徑別名設置
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  plugins: [vue()],
-});
+    plugins: [vue()],
+    // 本地反向代理，解決瀏覽器跨域限制
+    server: {
+      host: 'localhost',
+      port: Number(env.VITE_APP_PORT),
+      open: true, // 運行自動打開瀏覽器
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp('^' + env.VITE_APP_BASE_API), ''),
+        },
+      },
+    },
+  });
+};
