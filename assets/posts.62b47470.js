d3ef70acb4e940706ddbb8f04a665eb9f61b87cf
joko751214 <joko751214@gmail.com>
2022-05-12 08:10:08 +0800

    deploy

---
+++ assets/posts.62b47470.js
@@ -0,0 +1,1 @@
+import{s as o}from"./request.17c9c2ef.js";function r(t){return o({url:"/posts",method:"get",params:t})}function e(t){return o({url:"/posts",method:"post",data:t})}export{r as g,e as p};
