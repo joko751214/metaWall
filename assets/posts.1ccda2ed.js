9d34b0a4daa063160ad1ba0cef1c56a08f31781b
joko751214 <joko751214@gmail.com>
2022-05-14 10:11:02 +0800

    deploy

---
+++ assets/posts.1ccda2ed.js
@@ -0,0 +1,1 @@
+import{s as o}from"./request.e398defe.js";function r(t){return o({url:"/posts",method:"get",params:t})}function e(t){return o({url:"/posts",method:"post",data:t})}export{r as g,e as p};
--- assets/posts.e5ec62cc.js
+++
@@ -1,1 +0,0 @@
-import{s as o}from"./request.e11f8cfb.js";function r(t){return o({url:"/posts",method:"get",params:t})}function e(t){return o({url:"/posts",method:"post",data:t})}export{r as g,e as p};
